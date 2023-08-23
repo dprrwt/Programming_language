4581ca72151bb6fd5a7dd5e53a0f8d664633e696
dprrwt <dprrawat296@gmail.com>
2023-08-23 15:01:40 +0530

    module practice

---
+++ Python/module.py
@@ -0,0 +1,6 @@
+#what is module: a package of set of files such that it can be used again 
+# Import datetime from datetime below:
+from datetime import datetime
+
+current_time = datetime.now()
+print(current_time)
