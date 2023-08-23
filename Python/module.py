f97f0e07e0870606dde63bc4052c680279116e1e
dprrwt <dprrawat296@gmail.com>
2023-08-23 15:42:15 +0530

    module pracitce

--- Python/module.py
+++ Python/module.py
@@ -4,3 +4,16 @@ from datetime import datetime
 
 current_time = datetime.now()
 print(current_time)
+
+##random module
+# Import random below:
+import random
+
+# Create random_list below:
+random_list = [random.randint(0, num) for num in range(101)]
+
+# Create randomer_number below:
+randomer_number = random.choice(random_list)
+
+# Print randomer_number below:
+print(randomer_number)
